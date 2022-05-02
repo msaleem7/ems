0bdb91dec94e20388a5f2debe7fcb126f4ff83ff
msaleem7 <msaliim7@gmail.com>
2022-05-02 16:02:48 +0500

    added routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :org_admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'org_admin/events#index'
+
+  namespace :org_admin do
+    resources :events, only: %w(index new edit)
+  end
+
+  namespace :api do
+    namespace :org_admin do
+      resources :events, only: %w(create show index update destroy)
+    end
+  end
 end
