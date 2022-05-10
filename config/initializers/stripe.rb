2c566bda8666f15b8a858ef3f6f5f03194909a1b
msaleem7 <msaliim7@gmail.com>
2022-05-10 16:21:33 +0500

    added stripe keys

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+Stripe.api_key = ENV['STRIPE_SECRET']
+
+# source: https://wiki.xmldation.com/Support/EPC/List_of_SEPA_countries
+SEPA_COUNTRIES = %w(
+  FI AT PT BE BG HR CY CZ DK EE FR GF DE GI GR GP GG HU IS IE IM IT JE LV LI LT
+  LU MT MQ YT MC NL NO PL RE RO BL MF PM SM SK SI ES SE CH GB VA AD US
+)
